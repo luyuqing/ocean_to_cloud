e150843b3fdc9939408e0b5c838de92d4f02993d
Yuqing Lu <luyuqing0708@gmail.com>
2018-06-11 20:27:18 +0200

    Delete __init__.py

--- compute/__init__.py
+++
@@ -1,4 +0,0 @@
-from compute.bursting import cal_pressure_containment
-from compute.collapse import cal_collaps
-from compute.propgation_buckling import cal_prop_buckling
-from compute.reeling import cal_reeling
